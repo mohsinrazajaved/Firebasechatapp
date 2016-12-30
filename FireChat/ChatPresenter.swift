4e8c0c5a53fe9515da8af286d31e3f0bdb495adc
mohsinrazajaved <mohsinrazajaved444@gmail.com>
2016-12-30 14:28:15 +0500

    kmh

---
+++ FireChat/ChatPresenter.swift
@@ -0,0 +1,19 @@
+//
+//  ChatPresenterr.swift
+//  FireChat
+//
+//  Created by mohsin raza on 30/12/2016.
+//  Copyright © 2016 mohsin raza. All rights reserved.
+//
+
+import Foundation
+
+class ChatPresenterr
+{
+    
+    var delegate:ViewDelegate?
+    var datasource:ViewDataSource?
+    private let fire_contact = FireContacts()
+
+    
+}
